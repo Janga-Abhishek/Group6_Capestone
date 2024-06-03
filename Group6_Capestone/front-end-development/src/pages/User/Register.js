b33f11e9fbf7fac209721a82d95f6baf1f0fc18f
Revathi Sasi <revathysasi01@gmail.com>
2024-06-03 14:24:32 -0400

    Added validation for registration page.

--- Group6_Capestone/front-end-development/src/pages/User/Register.js
+++ Group6_Capestone/front-end-development/src/pages/User/Register.js
@@ -53,7 +53,7 @@ const containerStyle = {
   alignItems: "center",
   backgroundImage: "url('/images/login_background.png')",
   backgroundSize: "cover",
-  height: "130vh",
+  height: "150vh",
   width: "100%",
 };
 
@@ -66,15 +66,17 @@ const logoStyle = {
   animation: "rotateCoin 3s infinite",
 };
 
-
-const handleRegistration = (e) =>{
-  e.preventDefault();
-  // Add validation and registration logic here
-  console.log("form");
- // navigate("/login"); // Redirect to login page after successful registration
+const errorStyle = {
+  color: "red",
+  fontSize: "0.8em",
+  float: "left",
 };
 
+
+
+
 const Register = () => {
+
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
@@ -84,6 +86,133 @@ const Register = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [rePassword, setRePassword] = useState("");
   const [isButtonHovered, setIsButtonHovered] = useState(false);
+
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [insuranceNumberError, setInsuranceNumberError] = useState("");
+  const [addressError, setAddressError] = useState("");
+  const [phoneNumberError, setPhoneNumberError] = useState("");
+  const [rePasswordError, setRePasswordError] = useState("");
+
+
+  const validateForm = (e)=>{
+    //empty checking
+    if(!firstName){
+      setFirstNameError("First Name is required!");
+      
+    }
+    if(!lastName){
+      setLastNameError("Last Name is required!");
+      
+    }
+    if(!email){
+      setEmailError("Email is required!");
+      
+    }
+    if(!phoneNumber){
+      setPasswordError("Phone Number is required!");
+      
+    }
+    if(!password){
+      setPasswordError("Password is required!");
+      
+    }
+    if(!rePassword){
+      setRePasswordError("Confirm Password is required!");
+      
+    }
+    if(!address){
+      setAddressError("Address is required!");
+      
+    }
+    if(!insuranceNumber){
+      setInsuranceNumberError("Health Insurance Number is required!");
+      
+    }
+
+    //password strength & matching
+
+    const validatePassword = (password) => {
+      const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}$/;
+      return regex.test(password);
+    };
+
+    if (validatePassword(password)) {
+      setPasswordError('');
+    } else {
+      setPasswordError('Password must be at least 8 characters long, contain at least one number, one uppercase letter, one lowercase letter, and one special character.');
+    }
+
+    if(password === rePassword){
+      setRePasswordError("");
+    }else{
+      setRePasswordError("Password do not match !");
+    }
+    //validation for name
+    const validateName = (name) => {
+      const regex = /^[A-Za-z\s]{2,50}$/;
+      return regex.test(name);
+    };
+
+    if (validateName(firstName)) {
+      setFirstNameError('');
+    } else {
+      setFirstNameError('First Name must be 2-50 characters long and contain only letters and spaces.');
+    }
+
+    if (validateName(lastName)) {
+      setLastNameError('');
+    } else {
+      setLastNameError('Last Name must be 2-50 characters long and contain only letters and spaces.');
+    }
+
+    //validation for email id
+
+    const validateEmail = (email) => {
+      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return regex.test(email);
+    };
+
+    if(validateEmail(email)){
+      setEmailError("");
+    }else{
+      setEmailError("Invalid email address");
+    }
+
+    //validation for phone number
+
+    const validatePhone = (phone) => {
+      const regex = /^(\+1)?\d{10}$/;
+      return regex.test(phone);
+    };
+
+    if(validatePhone(phoneNumber)){
+      setPhoneNumberError("");
+    }else{
+      setPhoneNumberError("Invalid phone number !")
+    }
+  //validate health insuarance number
+
+  const validateHealthCardNumber= (no)=>{
+    const  regex = /^(?:\d{9}|\d{10}|[A-Z]\d{11})$/;
+    return regex.test(no);
+  }
+  if (validateHealthCardNumber(insuranceNumber)){
+    setInsuranceNumberError("");
+  }else{
+    setInsuranceNumberError("Invalid Insurance number !");
+
+  }
+  }
+
+  const handleRegistration = async (e) =>{
+    e.preventDefault();
+    validateForm();
+    console.log("form");
+   // navigate("/login"); // Redirect to login page after successful registration
+  };
 
  return(
   <div>
@@ -97,57 +226,92 @@ const Register = () => {
             placeholder="First Name"
             style={inputStyle}
             value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
+            onChange={(e) => {
+              setFirstName(e.target.value);
+              setFirstNameError("");
+            }}
           />
+          <span style={errorStyle}>{firstNameError}</span>
           <input
             type="text"
             placeholder="Last Name"
             style={inputStyle}
             value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
+            onChange={(e) => {
+              setLastName(e.target.value);
+              setLastNameError("");
+            }}
           />
+          <span style={errorStyle}>{lastNameError}</span>
           <input
             type="text"
             placeholder="Email"
             style={inputStyle}
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              setEmailError("");
+            }}
           />
+          <span style={errorStyle}>{emailError}</span>
           <input
             type="text"
             placeholder="Phone Number"
             style={inputStyle}
             value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
+            onChange={(e) => {
+              setPhoneNumber(e.target.value);
+              setPhoneNumberError("");
+
+            }}
           />
+          <span style={errorStyle}>{phoneNumberError}</span>
           <input
             type="password"
             placeholder="Password"
             style={inputStyle}
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => {
+              setPassword(e.target.value);
+              setPasswordError("");
+            }}
           />
+          <span style={errorStyle}>{passwordError}</span>
           <input
             type="password"
             placeholder="Confirm Password"
             style={inputStyle}
             value={rePassword}
-            onChange={(e) => setRePassword(e.target.value)}
+            onChange={(e) => {
+              setRePassword(e.target.value);
+              setRePasswordError("");
+
+            }}
           />
+          <span style={errorStyle}>{rePasswordError}</span>
           <input
             type="text"
             placeholder="Address"
             style={inputStyle}
             value={address}
-            onChange={(e) => setAddress(e.target.value)}
+            onChange={(e) => {
+              setAddress(e.target.value);
+              setAddressError("");
+            }}
           />
+          <span style={errorStyle}>{addressError}</span>
           <input
             type="text"
             placeholder="Health Insurance Number"
             style={inputStyle}
             value={insuranceNumber}
-            onChange={(e) => setInsuranceNumber(e.target.value)}
+            onChange={(e) => {
+              setInsuranceNumber(e.target.value);
+              setInsuranceNumberError("");
+            }}
           />
+          <span style={errorStyle}>{insuranceNumberError}</span>
+
           <button
             type="submit"
             style={{ ...buttonStyle, ...(isButtonHovered && buttonHoverStyle) }}
