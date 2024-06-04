36d5c773df9eee6d9fd4c41242c6bc5fa3423378
sunny kasuvojula <sunnykrishank12@gmail.com>
2024-06-03 21:34:25 -0400

    add admin menu and dashboard components

---
+++ Group6_Capestone/front-end-development/src/components/AdminMenu.js
@@ -0,0 +1,7 @@
+export default function AdminMenu(){
+    return(
+        <div>
+            <h1>This is the admin menu</h1>
+        </div>
+    )
+}
---
+++ Group6_Capestone/front-end-development/src/pages/Admin/AdminDashboard.js
@@ -0,0 +1,7 @@
+export default function AdminDashboard(){
+    return(
+        <div>
+            <h1>This is the admin dash board</h1>
+        </div>
+    )
+}
